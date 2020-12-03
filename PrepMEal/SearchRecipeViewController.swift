68300f0253a3bf972aff0064379cc51fae660c28
Joanne Fung <joannefung120@gmail.com>
2020-12-02 20:33:41 -0500

    Fixed search recipe button, added search bar and retrieve data from search bar

--- PrepMEal/SearchRecipeViewController.swift
+++ PrepMEal/SearchRecipeViewController.swift
@@ -7,15 +7,20 @@
 
 import UIKit
 
-class SearchRecipeViewController: UITableViewController {
+class SearchRecipeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 54, left: 0, bottom: 0, right: 0)
     }
     
-    //MARK:- Actions
-    @IBAction func add() {
-        navigationController?.popViewController(animated: true)
-    }
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
+}
+
+extension SearchRecipeViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: '\(searchBar.text!)'")
+    }
 }
